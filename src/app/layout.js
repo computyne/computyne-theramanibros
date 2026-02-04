650cf6951901c7f0a410929ef48dec1d193ad890
GPC Yash <gpc.yashdeveloper@gmail.com>
2026-02-04 17:33:20 +0530

    banner and mobile menu issue solved

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,34 +43,23 @@ export default function RootLayout({children}) {
         <html lang="en" data-scroll-behavior="smooth" dir="ltr">
 
         <head>
-            {/* Google Tag Manager */}
-            <Script strategy="beforeInteractive">
-                {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),
-                  dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KFWT3PN');
-          `}
-            </Script>
+          <Script
+            src="https://www.googletagmanager.com/gtm.js?id=GTM-KFWT3PN"
+            strategy="beforeInteractive"
+          />
         </head>
 
         <body className={`${bodyFont.variable} ${headingFont.variable}`}>
 
         {/* Google Tag Manager (noscript) */}
-               <noscript>
-                 <iframe
-                   src="https://www.googletagmanager.com/ns.html?id=GTM-KFWT3PN"
-                   height="0"
-                   width="0"
-                   style={{ display: "none", visibility: "hidden" }}
-                 />
-               </noscript>
+              <noscript>
+                <iframe
+                  src="https://www.googletagmanager.com/ns.html?id=GTM-KFWT3PN"
+                  height="0"
+                  width="0"
+                  style={{ display: "none", visibility: "hidden" }}
+                />
+              </noscript>
                {/* End Google Tag Manager (noscript) */}
 
         {children}
