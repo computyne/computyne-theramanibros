46f1be4e9e4de45b40811783f8befff5b37725d5
GPC Yash <gpc.yashdeveloper@gmail.com>
2026-02-05 17:34:10 +0530

    Revert "header changes"
    This reverts commit 21df81cd

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,20 +43,10 @@ export default function RootLayout({children}) {
         <html lang="en" data-scroll-behavior="smooth" dir="ltr">
 
         <head>
-          {/* Google Tag Manager */}
-          <Script id="gtm-script" strategy="beforeInteractive">
-              {`
-          (function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),
-                dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KFWT3PN');
-        `}
+          <Script
+            src="https://www.googletagmanager.com/gtm.js?id=GTM-KFWT3PN"
+            strategy="beforeInteractive"
+          />
         </head>
 
         <body className={`${bodyFont.variable} ${headingFont.variable}`}>
