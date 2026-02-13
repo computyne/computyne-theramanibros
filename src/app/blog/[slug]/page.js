a123dd66254f6fa821dbe53acf48bd50ec5cffcc
GPC Yash <gpc.yashdeveloper@gmail.com>
2026-02-13 22:38:56 +0530

    added schema and meta data in blog

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -7,24 +7,55 @@ import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
 import getBlogs from "@/libs/getBlogs";
 import { notFound } from "next/navigation";
 import SubFooter from "@/components/layout/footer/subFooter";
+import getSchemaByPathNdSlug from "@/libs/getSchemaByPathNdSlug";
+import JsonLd from "@/components/seo/JsonLd";
 
 const items = getBlogs();
+
+export async function generateMetadata({ params }) {
+  const { slug } = await params;
+
+  const currentItem = items.find((item) => item.slug === slug);
+
+  if (!currentItem) return {};
+
+  return {
+    title: currentItem.meta.metaTitle,
+    description: currentItem.meta.metaDescription,
+    keywords: currentItem.meta.keywords,
+    alternates: {
+      canonical: `https://www.computyne.com/blog/${slug}`,
+    },
+  };
+}
+
 
 export default async function BlogDetails({ params }) {
     const { slug } = await params;
 
     const currentItem = items.find((item) => item.slug === slug);
 
+      const schema = await getSchemaByPathNdSlug(
+        "blog-schema",
+        slug
+    );
+
+
     if (!currentItem) {
         notFound();
     }
 
-    return (
-        <div>
-            <BackToTop />
-            <Header headerType={5} />
-            <Header headerType={5} isStickyHeader={true} />
-            {/* <div id="smooth-wrapper">
+   return (
+       <>
+           <JsonLd
+               id={`${slug}-schema`}
+               schema={schema}
+           />
+           <div>
+               <BackToTop />
+               <Header headerType={5} />
+               <Header headerType={5} isStickyHeader={true} />
+               {/* <div id="smooth-wrapper">
                 <div id="smooth-content">
                     <main>
                         <div className="top-space-15"></div>
@@ -34,15 +65,16 @@ export default async function BlogDetails({ params }) {
                     <Footer/>
                 </div>
             </div> */}
-            <main>
-                <div className="top-space-15"></div>
-                <BlogDetailsMain currentItem={currentItem} />
-                <SubFooter />
-            </main>
-            <Footer />
+               <main>
+                   <div className="top-space-15"></div>
+                   <BlogDetailsMain currentItem={currentItem} />
+                   <SubFooter />
+               </main>
+               <Footer />
 
-            <ClientWrapper />
-        </div>
+               <ClientWrapper />
+           </div>
+       </>
     );
 }
 
