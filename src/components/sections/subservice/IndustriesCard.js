4e898c20174f9df6c2e37a5b5a8a3d02288ab124
Akash Ramani <akashr@codemechsolutions.com>
2026-02-14 15:00:04 +0530

    mobile responsive issue solved

--- src/components/sections/subservice/IndustriesCard.js
+++ src/components/sections/subservice/IndustriesCard.js
@@ -5,7 +5,7 @@ const IndustriesCard = ({industry, idx}) => {
     const isValidUrl = !!url && url.trim() !== "" && url.trim() !== "#";
     return (
         <div className="blog-item style-2">
-            <div className="blog-thumb wd-30">
+            <div className="blog-thumb wd-30-custom">
                 <img src={img ? img : "/images/blog/blog-4.webp"} alt=""/>
             </div>
             <div className="blog-content">
